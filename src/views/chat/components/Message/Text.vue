3a7ad92700177d8a601ec29bdcb53c59d58e92c6
tangbo <admin@tbphp.net>
2023-03-22 12:15:47 +0800

    feat:修改代码样式

--- src/views/chat/components/Message/Text.vue
+++ src/views/chat/components/Message/Text.vue
@@ -55,7 +55,7 @@ const text = computed(() => {
 })
 
 function highlightBlock(str: string, lang?: string) {
-  return `<pre class="code-block-wrapper"><div class="code-block-header"><span class="code-block-header__lang">${lang}</span><span class="code-block-header__copy">${t('chat.copyCode')}</span></div><code class="hljs code-block-body ${lang}">${str}</code></pre>`
+  return `<pre class="code-block-wrapper"><div class="code-block-header"><span class="code-block-header__lang">${lang}</span><span class="code-block-header__copy">${t('chat.copyCode')}</span></div><code class="hljs code-block-body ${lang}">${str}</code></pre><div class="code-block-newline"></div>`
 }
 
 defineExpose({ textRef })
